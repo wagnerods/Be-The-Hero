ea6b968a903830ea8e8de05731df56cc7dc98136
Wagner Da Silva <Wagner.DaSilva@hyland.com>
2020-03-28 11:50:13 -0300

    BACKEND

---
+++ src/index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const cors = require('cors');
+const { errors } = require('celebrate')
+const routes = require('./routes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.use(errors());
+
+//#region Aulas sobre Metodos HTTP ROTA E RECURSOS
+/**
+ * Rota / Recurso 
+ */
+
+/**
+ * Métodos HTTP:
+ * 
+ * GET: Buscar/Listar uma informação do back-end
+ * POST: Criar uma informação no back-end
+ * PUT: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end
+ */
+
+ /**
+  * Tipos de parâmtros:
+  * 
+  * Query Params: Parâmetros nomeados enviados na rota após "?" (Filtros, paginação)
+  * Route Params: Parâmetros utilizados para identificar recursos
+  * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
+  */
+
+  /**
+   * SQL: MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL Server
+   * NoSQL: MongoDB, CouchDB, etc
+   */
+
+   /** 3 Formas de instalar o BANCO
+    * Driver: SELECT * FROM users
+    * Query Builder: table('users').select('*').where() Utiliza linguagem Java Script
+    *  
+    */
+//#endregion
+ 
+app.listen(3333);
+
